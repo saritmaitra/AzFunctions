e6db6d7e20bef92a184ed3a321ff9ad49aeef65e
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-14 21:38:48 +0800

    Update logging.py

--- logging-info/logging.py
+++ logging-info/logging.py
@@ -1,4 +1,5 @@
 import logging
+import employee
 
 # DEBUG: Detailed information, typically of interest only when diagnosing problems.
 
@@ -32,6 +33,64 @@ def multiply(x, y):
 def divide(x, y):
     """Divide Function"""
     return x / y
+  
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('sample.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        logger.exception('Tried to divide by zero')
+    else:
+        return result
+
+
+num_1 = 10
+num_2 = 0
+
+add_result = add(num_1, num_2)
+logger.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+
+sub_result = subtract(num_1, num_2)
+logger.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+
+mul_result = multiply(num_1, num_2)
+logger.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+
+div_result = divide(num_1, num_2)
+logger.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
 
 
 num_1 = 20
